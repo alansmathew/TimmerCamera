469a8ca32cc4487274cd33b790abff0e04445d47
Alan S Mathew <43809019+alansmathew008@users.noreply.github.com>
2019-06-10 12:49:32 +0530

    Add files via upload
    Swift

---
+++ Timer camera/Timer camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer camera
+//
+//  Created by Alan S Mathew on 10/06/19.
+//  Copyright © 2019 Alan S Mathew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
