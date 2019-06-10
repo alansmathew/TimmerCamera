b94ea3043e5a4b5b612cfd184c7afbdabd0d8810
Alan S Mathew <43809019+alansmathew008@users.noreply.github.com>
2019-06-10 17:31:04 +0530

    Add files via upload

--- Timer camera/Timer camera/ViewController.swift
+++ Timer camera/Timer camera/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var backCamera : AVCaptureDevice?
+    var frontCamera : AVCaptureDevice?
+    var currentCamera : AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    var cameraPerviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPerviewLayer()
+        startRunningCaptureSession()
         // Do any additional setup after loading the view.
     }
     
+    func setupCaptureSession(){
+        captureSession.sessionPreset=AVCaptureSession.Preset.photo
+    }
+    func setupDevice(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            }else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        currentCamera = backCamera
+    }
     
+    func setupInputOutput(){
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format:[AVVideoCodecKey: AVVideoCodecType.jpeg])],completionHandler: nil)
+            
+        }catch {
+            print(error)
+        }
+    }
+    func setupPerviewLayer(){
+        cameraPerviewLayer =  AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPerviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPerviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPerviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPerviewLayer!,at :0)
+    }
+    func startRunningCaptureSession(){
+        captureSession.startRunning()
+    }
 }
 
